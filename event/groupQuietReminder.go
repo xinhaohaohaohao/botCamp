1149562a322ff01363fbefce5470717ab3f9866a
zxh <xinhao177437@163.com>
2023-02-18 15:51:09 +0800

    commit

---
+++ event/groupQuietReminder.go
@@ -0,0 +1,40 @@
+package event
+
+import (
+	"botcamp/botDemo/api"
+	lark "github.com/larksuite/oapi-sdk-go/v3"
+	"time"
+)
+
+var content = "群消息安静提醒"
+
+// GroupQuietReminder 群消息安静提醒
+func GroupQuietReminder(client *lark.Client, timeInterval float64, receiveId string) {
+	//var receiveId string
+	//receiveId = <-receiveIdChan
+	//fmt.Println(receiveId)
+	for true {
+		lastMessageTime := api.GetLastMessageTime(client, receiveId)
+		diffTime := getLastToNowDiffTime(lastMessageTime)
+		if diffTime > timeInterval {
+			err := api.SentMessageToGroup(client, "chat_id", receiveId, content)
+			if err != nil {
+				panic(err)
+			}
+		}
+	}
+
+}
+
+// 获取当前时间距最后一次消息的时间差  秒钟级别
+func getLastToNowDiffTime(lastMessageTimeStr string) float64 {
+	messageTime := stringToTime(lastMessageTimeStr)
+	t := time.Now().Sub(messageTime)
+	return t.Seconds()
+}
+
+func stringToTime(timeStr string) time.Time {
+	timeTemplate := "2006-01-02 15:04:05"
+	timeTime, _ := time.ParseInLocation(timeTemplate, timeStr, time.Local)
+	return timeTime
+}
---
+++ event/groupQuietReminder_test.go
@@ -0,0 +1,11 @@
+package event
+
+import (
+	"testing"
+)
+
+func TestGroupQuietReminder(t *testing.T) {
+	//var appId, appSecret = "cli_a36708e0b038900e", "Tv3X6wFT0CgyIDX5NgZ8VhUF1VTaKO1n"
+	//client := lark.NewClient(appId, appSecret)
+
+}
